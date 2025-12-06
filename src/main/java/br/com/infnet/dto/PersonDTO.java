63f8132437594e872b681363f5231889c1d31faa
João Victor Pinheiro <127742336+joaovipinheiro@users.noreply.github.com>
2025-12-06 15:25:09 -0300

    Update PersonDTO.java

--- src/main/java/br/com/infnet/dto/PersonDTO.java
+++ src/main/java/br/com/infnet/dto/PersonDTO.java
@@ -1,12 +1,2 @@
 package br.com.infnet.dto;
-
-/**
- * Refatoração TP5: Uso de RECORD para garantir Imutabilidade.
- * Records não possuem setters, atendendo ao requisito de eliminar configuradores.
- */
-public record PersonDTO(
-        Long id,
-        String name,
-        String email,
-        String phone
-) {}
+public record PersonDTO(Long id, String name, String email, String phone) {}
